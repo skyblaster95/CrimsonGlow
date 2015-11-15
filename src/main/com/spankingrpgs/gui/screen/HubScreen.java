ab6db5cb7ed704fc156ce994ab9578df4ddd3427
Andrew Russell <sprpgs@gmail.com>
2015-11-15 12:31:16 -0600

    Created HubScreen
    - Used to display the event options open to the player.

---
+++ src/main/com/spankingrpgs/gui/screen/HubScreen.java
@@ -0,0 +1,24 @@
+package com.spankingrpgs.gui.screen;
+
+import javax.swing.*;
+
+/**
+ * Created by andrewRussell on 11/15/2015.
+ */
+public class HubScreen {
+    private JPanel canvas;
+    private JLabel title;
+    private JButton goOnPatrol;
+    private JButton party;
+    private JButton study;
+    private JButton train;
+    private JButton work;
+    private JPanel commands;
+    private JPanel optionsExit;
+    private JButton options;
+    private JButton exit;
+    private JPanel saveLoad;
+    private JButton save;
+    private JButton load;
+    private JPanel eventSelection;
+}
