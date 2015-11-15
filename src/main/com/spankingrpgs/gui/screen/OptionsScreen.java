cf5d08b4ebbb57fb6d522d9bfa8bf2ff73ceb807
Andrew Russell <sprpgs@gmail.com>
2015-11-15 12:28:59 -0600

    Created OptionsScreen

---
+++ src/main/com/spankingrpgs/gui/screen/OptionsScreen.java
@@ -0,0 +1,26 @@
+package com.spankingrpgs.gui.screen;
+
+import javax.swing.*;
+
+/**
+ * Created by andrewRussell on 11/15/2015.
+ */
+public class OptionsScreen {
+    private JPanel canvas;
+    private JLabel title;
+    private JPanel ai;
+    private JPanel attritionRate;
+    private JPanel enemiesCanSpank;
+    private JLabel aiLabel;
+    private JPanel aiOptions;
+    private JRadioButton average;
+    private JRadioButton dumb;
+    private JRadioButton smart;
+    private JLabel attritionLabel;
+    private JPanel attritionOptions;
+    private JRadioButton severe;
+    private JRadioButton moderate;
+    private JRadioButton none;
+    private JLabel enemiesCanSpankLabel;
+    private JPanel enemiesCanSpankChoices;
+}
